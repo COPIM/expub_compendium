7fc421d495e55319df6c72e8cb95b3d0e68b6050
Simon <ad7588@coventry.ac.uk>
2021-10-20 10:22:53 +0100

    Switched to Bootstrap

--- web/app/tool.py
+++ web/app/tool.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from .models import Tool
 from . import db
