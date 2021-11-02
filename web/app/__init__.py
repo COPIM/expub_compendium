96df4161e1e0c398070fe0e3e7d76fbd3d32d7b8
Simon <ad7588@coventry.ac.uk>
2021-11-02 10:57:47 +0000

    Added MariaDB config for production use

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -26,7 +26,8 @@ def create_app():
     # get config variables from OS environment variables: set in env file passed through Docker Compose
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
+    app.config['SQLALCHEMY_ECHO'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
