31113c6a89caec02a3bd063834cd1f7c3cb4c1fa
Simon <ad7588@coventry.ac.uk>
2022-05-13 13:52:56 +0100

    fixing subdomain redirection

--- web/app/auth.py
+++ web/app/auth.py
@@ -12,7 +12,7 @@ from flask_login import login_user, logout_user, login_required
 from .models import User
 from . import db
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint('auth', __name__, subdomain="toolkit")
 
 # routes for login page
 @auth.route('/login')
