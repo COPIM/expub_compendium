c86fd54a9995b959f30a3eabfa1ccad86dea96be
Simon <ad7588@coventry.ac.uk>
2023-04-06 15:41:33 +0100

    improvements to filters to allow multiple filters

--- web/app/book.py
+++ web/app/book.py
@@ -20,17 +20,19 @@ book = Blueprint('book', __name__)
 @book.route('/books')
 def get_books():
     view = request.args.get('view')
-    type = 'book'
-    books = Resource.query.filter_by(type=type).all()
+    resource_type = 'book'
+    books_query = Resource.query.filter_by(type=resource_type)
     for key in request.args.keys():
         if key != 'view':
             if key == 'practice':
-                books = Resource.query.join(Relationship, Relationship.first_resource_id == Resource.id, isouter=True).filter(Resource.type==type, Relationship.second_resource_id==request.args.get(key)).all()
-                also_books = Resource.query.join(Relationship, Relationship.second_resource_id == Resource.id, isouter=True).filter(Resource.type==type, Relationship.first_resource_id==request.args.get(key)).all()
-                books = books + also_books
-            else:
-                kwargs = {'type': type, key: request.args.get(key)}
-                books = Resource.query.filter_by(**kwargs).all()
+                books_1 = books_query.join(Relationship, Relationship.first_resource_id == Resource.id, isouter=True).filter(Relationship.second_resource_id==request.args.get(key))
+                books_2 = books_query.join(Relationship, Relationship.second_resource_id == Resource.id, isouter=True).filter(Relationship.first_resource_id==request.args.get(key))
+                books_query = books_1.union(books_2)
+            if key != 'practice':
+                kwargs = {key: request.args.get(key)}
+                books_query = books_query.filter_by(**kwargs)
+    # finalise the query
+    books = books_query.all()
     # get number of books
     count = len(books)
     if view != 'list':
@@ -39,14 +41,14 @@ def get_books():
     else: 
         # reorder books by book name
         books = sorted(books, key=lambda d: d.__dict__['name']) 
-    # get filters
+    # get values for filters
     # practices 
-    practices_filter = Resource.query.filter_by(type='practice').with_entities(Resource.id, Resource.name)
+    practices_filter = Resource.query.filter_by(type='practice').with_entities(Resource.id, Resource.name).all()
     # year
-    year_filter = get_filter_values('year', type)
+    year_filter = get_filter_values('year', resource_type)
     # typology
-    typology_filter = get_filter_values('typology', type)
-    return render_template('resources.html', resources=books, type=type, practices_filter=practices_filter, year_filter=year_filter, typology_filter=typology_filter, count=count, view=view)
+    typology_filter = get_filter_values('typology', resource_type)
+    return render_template('resources.html', resources=books, type=resource_type, practices_filter=practices_filter, year_filter=year_filter, typology_filter=typology_filter, count=count, view=view)
 
 # route for displaying a single book based on the ID in the database
 @book.route('/books/<int:book_id>')
