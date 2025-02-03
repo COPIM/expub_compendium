247f462a5ddef61df593c30383fe54ec2e2e5807
Simon <ad7588@coventry.ac.uk>
2025-02-03 11:59:26 +0000

    updating API for practices

--- web/app/schemas.py
+++ web/app/schemas.py
@@ -8,6 +8,9 @@
 # https://stackoverflow.com/questions/59721478/serializing-sqlalchemy-with-marshmallow
 
 from . import ma
+import os
+from marshmallow import Schema, fields, post_dump
+from flask_marshmallow import Marshmallow
 
 # schema for JSON transformation of User table via Marshmallow
 class UserSchema(ma.Schema):
@@ -23,8 +26,24 @@ class ToolSchema(ma.Schema):
 
 class PracticeSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'description', 'experimental', 'lessonsLearned', 'references')
+        fields = ('id', 'name', 'content')
         ordered = True
+
+    id = fields.Int(required=True)
+    name = fields.Str(required=True)
+    content = fields.Str(dump_only=True)  # Will store the Markdown content
+
+    @post_dump
+    def load_markdown(self, data, **kwargs):
+        practice_name = data['name'].replace(" ", "_")
+        # Reads the markdown content based on the name field after serialization.
+        file_path = f"content/practices/{practice_name}.md"  # Adjust the path as needed
+        if os.path.exists(file_path):
+            with open(file_path, "r", encoding="utf-8") as file:
+                data["content"] = file.read()
+        else:
+            data["content"] = "Markdown file not found."
+        return data
 
 class BookSchema(ma.Schema):
     class Meta:
